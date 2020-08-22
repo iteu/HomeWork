dda3d72f6569e30ed06e6ae4ac65a17a35403f9b
iteu <61872438+iteu@users.noreply.github.com>
2020-08-22 23:30:26 +0500

    HomeWork-3

--- main.js
+++ main.js
@@ -1,1 +1,65 @@
-alert ('Привет Мир!');
+const $btn = document.getElementById('btn-kick');
+
+const character = {
+    name: 'Pikachu',
+    defaultHP: 100,
+    damageHP: 100,
+    elHP: document.getElementById('health-character'),
+    elProgressbar: document.getElementById('progressbar-character'),
+}
+
+const enemy = {
+    name: 'Charmander',
+    defaultHP: 100,
+    damageHP: 100,
+    elHP: document.getElementById('health-enemy'),
+    elProgressbar: document.getElementById('progressbar-enemy'),
+}
+
+$btn.addEventListener('click', function() {
+    console.log('Kick');
+    changeHP(random(30), character);
+    changeHP(random(30), enemy);
+});
+
+function init() {
+    console.log('Start game');
+    renderHP(character);
+    renderHP(enemy);
+}
+
+function renderHP (person) {
+    renderHPLife(person);
+    renderProgressbarHP(person);
+}
+
+function renderHPLife(person) {
+    person.elHP.innerText = person.damageHP + ' / ' + person.defaultHP;
+}
+
+function renderProgressbarHP(person) {
+    person.elProgressbar.style.width = person.damageHP + '%';
+}
+
+function changeHP (count, person) {
+    if (person.damageHP < count) {
+        person.damageHP = 0;
+        alert('Бедный '+ person.name +' проиграл бой!');
+        $btn.disabled = true;
+    }
+    else {
+        person.damageHP -= count;
+    }
+
+    renderHP(person);
+}
+
+function random (num) {
+    return Math.ceil(Math.random() * num);
+}
+
+function button () {
+    
+}
+
+init();
